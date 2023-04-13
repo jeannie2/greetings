dec2723a41db05e0f3cf7041a5dde42188cf1cfe
jeannie <jnam820@gmail.com>
2023-04-13 11:37:36 +0800

    cron.jsx

--- src/pages/api/cron.jsx
+++ src/pages/api/cron.jsx
@@ -3,7 +3,6 @@
 
 export default async function Cron(req, res) {
   try {
-    console.log("TEST CRON") // eslint-disable-line
     /* const q = query(collection(db, 'greetings3'), where('message', '==', 'rainbow'))
     const querySnapshot = await getDocs(q)
     querySnapshot.forEach((doc) => {
@@ -13,10 +12,9 @@ export default async function Cron(req, res) {
       }
       console.log(data) // eslint-disable-line
     // mailer
-    })
+    }) */
     res.json('Job Completed')
     //  )
-    */
   } catch (err) {
     console.log(err) // eslint-disable-line
     return res.status(400).json(err)
