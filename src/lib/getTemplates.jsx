48eeaac7accbdad2abe56acf7ec54e7d8f1d055d
jeannie <jnam820@gmail.com>
2023-04-13 15:58:15 +0800

    test5

--- src/lib/getTemplates.jsx
+++ src/lib/getTemplates.jsx
@@ -2,13 +2,18 @@ import path from 'path'
 import fs from 'fs'
 
 const getTemplates = () => {
-  const structure = {}
+  // const structure = {}
 
   try {
-    const folders = fs.readdirSync(path.join(__dirname, '../api/templates'))
-    folders.forEach((folder) => {
+    // const folders = fs.readdirSync(path.join(__dirname, '../api/templates'))
+    /* folders.forEach((folder) => {
       structure[folder] = fs.readdirSync(path.join(__dirname, `../api/templates/${folder}`)).map((n) => n.split('.')[0])
-    })
+    }) */
+    const structure = {
+      bday: ['bday1', 'bday2', 'bday3', 'bday4'],
+      thankyou: ['thankyou1', 'thankyou2', 'thankyou3', 'thankyou4'],
+      congrats: ['congrats1', 'congrats2', 'congrats3']
+    }
     return structure
   } catch (err) {
     console.log(err) // eslint-disable-line
