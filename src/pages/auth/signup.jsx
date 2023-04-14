5b87f8d0333e17b38030ded0738127308eeee936
jeannie <jnam820@gmail.com>
2023-04-14 12:21:37 +0800

    tw

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -8,7 +8,9 @@ function PagesAuthSignup() {
       <div className="row">
         <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
           <h1 className="text-center">Sign up</h1>
-          <p>Register now to view all your scheduled cards in one place, and unlock hidden content!</p>
+          <br />
+          <p className="fst-italic text-center">Register to view all your cards in one place, and unlock hidden content!</p>
+          <br />
           <FormsAuthSignup />
         </div>
       </div>
